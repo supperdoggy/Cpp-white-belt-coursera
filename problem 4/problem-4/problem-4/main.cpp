86d7b1dc159c118c954c66729a39998a46105bb5
supperdoggy <supperspggy229@gmail.com>
2020-07-14 18:22:58 +0300

    created project

---
+++ problem 4/problem-4/problem-4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  problem-4
+//
+//  Created by Maks on 14.07.2020.
+//  Copyright © 2020 Maks. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
