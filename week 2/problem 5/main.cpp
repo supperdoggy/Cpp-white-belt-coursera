859287a1176d44754b1562ce715f7176ca7640ab
supperdoggy <supperspggy229@gmail.com>
2020-07-17 16:15:13 +0300

    solved 5th problem of second week. deleted xcode project for solution

---
+++ week 2/problem 5/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  problem-5
+//
+//  Created by Maks on 17.07.2020.
+//  Copyright © 2020 Maks. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void Reverse(vector<int>& v){
+    reverse(v.begin(),v.end());
+}
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
