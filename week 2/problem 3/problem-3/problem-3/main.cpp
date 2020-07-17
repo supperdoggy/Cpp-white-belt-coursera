6055f04267b278404eabbcd0c765b8d778e4fe7a
supperdoggy <supperspggy229@gmail.com>
2020-07-17 15:48:56 +0300

    first try solving 3 problem on the second week

---
+++ week 2/problem 3/problem-3/problem-3/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  problem-3
+//
+//  Created by Maks on 17.07.2020.
+//  Copyright © 2020 Maks. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+void UpdateIfGreater(int& first, int& second){
+    if (first>second)
+        second = first;
+}
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
